12adb34fde460718bc53c734e70f20ac47a7bba6
Alexey Nikiforov <alenikifor@yandex.ru>
2022-11-24 20:39:50 +0300

    added random values

--- src/test/java/testdata/TestData.java
+++ src/test/java/testdata/TestData.java
@@ -2,22 +2,40 @@ package testdata;
 
 import com.github.javafaker.Faker;
 
+import java.util.Random;
+
 public class TestData {
     Faker faker = new Faker();
+    Random random = new Random();
 
     public String firstName = faker.name().firstName();
     public String lastName = faker.name().lastName();
     public String email = faker.internet().emailAddress();
-    public String gender = "Male";
+    public String gender = randomGender();
     public String number = faker.phoneNumber().subscriberNumber(10);
     public String birthDay = String.valueOf(faker.number().numberBetween(1, 30));
-    public String birthMonth = "September";
+    public String birthMonth = randomMonth();
     public String birthYear = String.valueOf(faker.number().numberBetween(1920, 2022));
     public String subject = "Computer Science";
-    public String hobbies = "Music";
+    public String hobbies = randomHobbies();
     public String upPicture = "img/hw_4.jpg";
     public String address = faker.address().fullAddress();
     public String state = "Haryana";
     public String city = "Panipat";
 
+    public String randomGender() {
+        String[] arrayGenders = {"Male", "Female", "Other"};
+        return arrayGenders[random.nextInt(arrayGenders.length)];
+    }
+
+    public String randomHobbies() {
+        String[] arrayHobbies = {"Sports", "Reading", "Music"};
+        return arrayHobbies[random.nextInt(arrayHobbies.length)];
+    }
+
+    public String randomMonth() {
+        String[] arrayHobbies = {"January", "February", "March", "April", "May",
+                "June", "July", "August", "September", "October", "November", "December"};
+        return arrayHobbies[random.nextInt(arrayHobbies.length)];
+    }
 }
